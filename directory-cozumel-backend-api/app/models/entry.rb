a754070669f804f46591a1bafc376f3e01fa5c67
atxrenegade <harleighabel@gmail.com>
2019-11-15 23:57:37 -0500

    update polymorphic model

--- directory-cozumel-backend-api/app/models/entry.rb
+++ directory-cozumel-backend-api/app/models/entry.rb
@@ -1,9 +1,4 @@
 class Entry <
 	belongs_to :entryable, polymorphic: true
-	#polymorphic belongs to photo, listing, or review
-	#polymorphic has_many businesses through, photos, listings, and reviews
-	has_many :businesses, through: :listings
-	has_many :businesses, through: :reviews
-	has_many :businesses, through: :photos
 	belongs_to :category
 end
