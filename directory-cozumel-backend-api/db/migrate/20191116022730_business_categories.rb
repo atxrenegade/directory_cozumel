6841a063e16e7d3977c159b7ae2c57e9b8d7fa14
atxrenegade <harleighabel@gmail.com>
2019-11-15 21:30:57 -0500

    add business_categories join table

---
+++ directory-cozumel-backend-api/db/migrate/20191116022730_business_categories.rb
@@ -0,0 +1,8 @@
+class BusinessCategories < ActiveRecord::Migration[5.2]
+  def change
+		create_table :businesses_categories, id:false do |t|
+      t.integer :business_id
+      t.integer :category_id
+		end	
+  end
+end
