778dc4566055f81055fbbc3b921450a2aa95261b
atxrenegade <harleighabel@gmail.com>
2019-11-17 23:41:27 -0500

    add column to Images table

--- directory-cozumel-backend-api/db/schema.rb
+++ directory-cozumel-backend-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_16_022730) do
+ActiveRecord::Schema.define(version: 2019_11_18_043959) do
 
   create_table "admins", force: :cascade do |t|
     t.string "username"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2019_11_16_022730) do
     t.integer "business_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "contributor_email"
     t.index ["business_id"], name: "index_images_on_business_id"
   end
 
