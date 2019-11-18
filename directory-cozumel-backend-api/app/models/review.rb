6850b2625a5074d14475c6f669f0c73bc811dc5a
atxrenegade <harleighabel@gmail.com>
2019-11-18 18:20:16 -0500

    add Review#format_review

--- directory-cozumel-backend-api/app/models/review.rb
+++ directory-cozumel-backend-api/app/models/review.rb
@@ -5,4 +5,17 @@ class Review < ApplicationRecord
 	def business_name
 		self.business.name
 	end
+
+	def self.format_reviews(bus_id)
+		unformatted_collection = Review.where(business_id: bus_id)
+		formatted_collection  = unformatted_collection.map do |x|
+			review = {}
+			review["id"] = x.id
+			review["rating"] = x.rating
+			review["content"] = x.content
+			review["contributor"] = x.contributor
+			review["contributor_email"] = x.contributor_email
+			return review
+		end
+	end
 end
