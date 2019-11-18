241e92327bf556b76c182bae2794942197d25988
atxrenegade <harleighabel@gmail.com>
2019-11-17 23:38:45 -0500

    add create methods to seed database

--- directory-cozumel-backend-api/db/seeds.rb
+++ directory-cozumel-backend-api/db/seeds.rb
@@ -15,75 +15,75 @@ categories_list = [
 ]
 
 businesses_list_1 = [
-[ 'Hotel Zilla', 1 ],
-[ 'Kernel Hotel', 1 ],
-[ 'Verge Hotel', 1 ],
-[ 'Liquid Hotel', 1 ],
-[ 'Citywide Hotel', 1 ],
-[ 'Hotel Best', 1 ],
-[ 'Moonlight Hotel', 1 ],
-[ 'Hotel Artist', 1 ],
-[ 'Leather Hotel', 1 ],
-[ 'Protection Hotel', 1 ],
-[ 'Name Hotel', 1 ],
-[ 'Championship Hotel', 1 ],
-[ 'Exclusive Hotel', 1 ]
+[ 'Hotel Zilla', 'hotel' ],
+[ 'Kernel Hotel', 'hotel' ],
+[ 'Verge Hotel', 'hotel' ],
+[ 'Liquid Hotel', 'hotel' ],
+[ 'Citywide Hotel', 'hotel' ],
+[ 'Hotel Best', 'hotel' ],
+[ 'Moonlight Hotel', 'hotel' ],
+[ 'Hotel Artist', 'hotel' ],
+[ 'Leather Hotel', 'hotel' ],
+[ 'Protection Hotel', 'hotel' ],
+[ 'Name Hotel', 'hotel' ],
+[ 'Championship Hotel', 'hotel' ],
+[ 'Exclusive Hotel', 'hotel' ]
 ]
 businesses_list2 = [
-[ 'Shire Hotel', 1 ],
-[ 'Print Hotel', 1 ],
-[ 'Hotel Hawk', 1 ],
-[ 'Kid Hotel', 1 ],
-[ 'Pascal Hotel', 1 ],
-[ 'Hotel Storage', 1 ],
-[ 'Hardware Mate', 2 ],
-[ 'All Hardware', 2 ],
-[ 'FourStar Hardware', 2 ],
-[ 'Secret Hardware', 2 ],
-[ 'Insight Hardware', 2 ],
-[ 'Metropolis Hardware', 2 ],
-[ 'Intrepid Hardware', 2 ],
-[ 'Hardware Chef', 2 ],
-[ 'Link Hardware', 2 ],
-[ 'Swift Hardware', 2 ],
-[ 'Shift Hardware', 2 ],
-[ 'Capricorn Hardware', 2 ],
-[ 'Hardware Widget', 2 ],
-[ 'Hybrid Hardware', 2 ],
-[ 'Dodge Hardware', 2 ],
-[ 'Measure Hardware', 2 ],
-[ 'Hardware Department', 2 ],
-[ 'Kayak Hardware', 2 ],
-[ 'FineLine Hardware', 2 ],
-[ 'Mainline Hardware', 2 ],
-[ 'Gas Station Time', 3 ],
-[ 'Cupcake Gas Station', 3 ],
-[ 'Gas Station Candy', 3 ],
-[ 'EaglEeye Gas Station', 3 ],
-[ 'Compare Gas Station', 3 ],
-[ 'Objective Gas Station', 3 ],
-[ 'Shadow Gas Station', 3 ],
-[ 'Tiger Gas Station', 3 ],
-[ 'Gifted Gas Station', 3 ],
-[ 'Gas Station Thread', 3 ],
-[ 'Sunshine Gas Station', 3 ],
-[ 'Candy Store Bounce', 4 ],
-[ 'Pre Candy Store', 4 ],
-[ 'Corpus Candy Store', 4 ],
-[ 'Net Candy Store', 4 ],
-[ 'ZeroPoint Candy Store', 4 ],
-[ 'BlueDiamond Candy Store', 4 ],
-[ 'Bigfoot Candy Store', 4 ],
-[ 'OffRoad Candy Store', 4 ],
-[ 'Car Rentals Savers', 5 ],
-[ 'OneWorld Car Rentals', 5 ],
-[ 'Simulation Car Rentals', 5 ],
-[ 'Augmented Car Rentals', 5 ],
-[ 'Turbine Car Rentals', 5 ],
-[ 'Acrylic Car Rentals', 5 ],
-[ 'Strength Car Rentals', 5 ],
-[ 'OneMinute Car Rentals', 5 ],
-[ 'Car Rentals Mega', 5 ]
+[ 'Shire Hotel', 'hotel' ],
+[ 'Print Hotel', 'hotel' ],
+[ 'Hotel Hawk', 'hotel' ],
+[ 'Kid Hotel', 'hotel' ],
+[ 'Pascal Hotel', 'hotel' ],
+[ 'Hotel Storage', 'hotel' ],
+[ 'Hardware Mate', 'hardware store' ],
+[ 'All Hardware', 'hardware store' ],
+[ 'FourStar Hardware', 'hardware store' ],
+[ 'Secret Hardware', 'hardware store' ],
+[ 'Insight Hardware', 'hardware store' ],
+[ 'Metropolis Hardware', 'hardware store' ],
+[ 'Intrepid Hardware', 'hardware store' ],
+[ 'Hardware Chef', 'hardware store' ],
+[ 'Link Hardware', 'hardware store' ],
+[ 'Swift Hardware', 'hardware store' ],
+[ 'Shift Hardware', 'hardware store' ],
+[ 'Capricorn Hardware', 'hardware store' ],
+[ 'Hardware Widget', 'hardware store' ],
+[ 'Hybrid Hardware', 'hardware store' ],
+[ 'Dodge Hardware', 'hardware store' ],
+[ 'Measure Hardware', 'hardware store' ],
+[ 'Hardware Department', 'hardware store' ],
+[ 'Kayak Hardware', 'hardware store' ],
+[ 'FineLine Hardware', 'hardware store' ],
+[ 'Mainline Hardware', 'hardware store' ],
+[ 'Gas Station Time', 'gas station' ],
+[ 'Cupcake Gas Station', 'gas station' ],
+[ 'Gas Station Candy', 'gas station' ],
+[ 'EaglEeye Gas Station', 'gas station' ],
+[ 'Compare Gas Station', 'gas station' ],
+[ 'Objective Gas Station', 'gas station' ],
+[ 'Shadow Gas Station', 'gas station' ],
+[ 'Tiger Gas Station', 'gas station' ],
+[ 'Gifted Gas Station', 'gas station' ],
+[ 'Gas Station Thread', 'gas station' ],
+[ 'Sunshine Gas Station', 'gas station' ],
+[ 'Candy Store Bounce', 'candy store' ],
+[ 'Pre Candy Store', 'candy store' ],
+[ 'Corpus Candy Store', 'candy store' ],
+[ 'Net Candy Store', 'candy store' ],
+[ 'ZeroPoint Candy Store', 'candy store' ],
+[ 'BlueDiamond Candy Store', 'candy store' ],
+[ 'Bigfoot Candy Store', 'candy store' ],
+[ 'OffRoad Candy Store', 'candy store' ],
+[ 'Car Rentals Savers', 'car rentals' ],
+[ 'OneWorld Car Rentals','car rentals' ],
+[ 'Simulation Car Rentals', 'car rentals' ],
+[ 'Augmented Car Rentals', 'car rentals' ],
+[ 'Turbine Car Rentals', 'car rentals' ],
+[ 'Acrylic Car Rentals', 'car rentals' ],
+[ 'Strength Car Rentals', 'car rentals' ],
+[ 'OneMinute Car Rentals', 'car rentals' ],
+[ 'Car Rentals Mega', 'car rentals' ]
 ]
 
 listings_list = [
@@ -92,7 +92,127 @@ listings_list = [
 	"address": "Calle 22 de Enero No. 261, Cozumel",
 	"phone_number": "123-456-7890",
 	"website": "www.example.com",
-	"business_id": 67
+	"business_id": 1
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 2
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 3
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 4
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 4
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 5
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 7
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 8
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 9
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 10
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 11
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 12
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 13
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 14
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 15
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 16
+},
+{
+"rating": 3 ,
+"address": "Calle 22 de Enero No. 261, Cozumel",
+"phone_number": "123-456-7890",
+"website": "www.example.com",
+"business_id": 17
+},
+
+	{
+	"rating": 3 ,
+	"address": "Calle 22 de Enero No. 261, Cozumel",
+	"phone_number": "123-456-7890",
+	"website": "www.example.com",
+	"business_id": 18
 },
 
 	{
@@ -100,79 +220,79 @@ listings_list = [
 	"address": "Ave. Rafael E. Melgar, #131",
 	"phone_number": "123-456-7890",
 	"website": "www.example.com",
-	"business_id": 6
-},
+	"business_id": 19
+	},
 
 	{
 	"rating": 5 ,
 	"address": "Km 11 Carreter Transversal, Cozumel",
 	"phone_number": "123-456-7890",
 	"website": "www.example.com",
-	"business_id": 15
-},
+	"business_id": 20
+	},
 
 	{
 	"rating": 1 ,
 	"address": "Punta Langosta Pier, Ave. Rafael E. Melgar, #499",
 	"phone_number": "123-456-7890",
 	"website": "www.example.com",
-	"business_id": 27
-},
+	"business_id": 21
+	},
 
 	{
 	"rating": 4 ,
 	"address": "International Pier, San Miguel, Cozumel",
 	"phone_number": "123-456-7890",
 	"website": "www.example.com",
-	"business_id": 45
-},
+	"business_id": 22
+	},
 
 	{
 	"rating": 4,
 	"address": "10 A Sur 198 Centro, San Miguel, Cozumel",
 	"phone_number": "123-456-7890",
 	"website": "www.example.com",
-	"business_id": 53
-},
+	"business_id": 23
+	},
 
 	{
 	"rating": 5,
 	"address": "Av. Rafael E. Melgar, #261, San Miguel, Cozumel",
 	"phone_number": "123-456-7890",
 	"website": "www.example.com",
-	"business_id": 22
-},
+	"business_id": 24
+	},
 
 	{
 	"rating": 1,
 	"address": "Royal Village Shopping Center, Ave. Rafael E. Melgar, San Miguel, Cozumel",
 	"phone_number": "123-456-7890",
 	"website": "www.example.com",
-	"business_id": 34
-},
+	"business_id": 25
+	},
 
 	{
 	"rating": 3,
 	"address": "Ave. Rafael E. Melgar at Adolfo Rosada Salas, San Miguel, Cozumel",
 	"phone_number": "123-456-7890",
 	"website": "www.example.com",
-	"business_id": 29
-},
+	"business_id": 26
+	},
 
 	{
 	"rating": 3,
 	"address": "Av. Rafael E. Melgar, #33, San Miguel, Cozumel",
 	"phone_number": "123-456-7890",
 	"website": "www.example.com",
-	"business_id": 30
-},
+	"business_id": 27
+	},
 
 	{
 	"rating": 2,
 	"address": "Ave. Rafael E. Melgar, Esquina Calle 2 Norte",
 	"phone_number": "123-456-7890",
 	"website": "www.example.com",
-	"business_id": 51
+	"business_id": 28
 	}
 ]
 
@@ -195,3 +315,15 @@ end
 listings_list.each do |rating, phone_number, address, website, business_id |
 	Listing.create(rating: rating, phone_number: phone_number, address: address, website: website, business_id: business_id)
 end
+
+reviews_list.each do |content, contributor, contributor_email, rating, business_id|
+	Review.create(content: content, contributor:contrbutor, contributor_email: contributor_email, rating: rating, business_id: business_id)
+end
+
+images_list.each do |description, date, url, contributor, constibutor_email, business_id|
+	Image.create(description: description, date: date , url: url, contributor: contributor, constibutor_email: contributor_email, business_id: business_id)
+end
+
+maps_list.each do |map_coords, business_id|
+	Map.create(map_coords: map_coords, business_id: business_id)
+end		
