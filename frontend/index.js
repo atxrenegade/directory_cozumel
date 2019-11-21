4852df786ac6b7b9f04833b771dd74afb687ef72
atxrenegade <harleighabel@gmail.com>
2019-11-21 00:47:42 -0500

    finish js toggle functionality for all forms and button

--- frontend/index.js
+++ frontend/index.js
@@ -18,14 +18,34 @@ window.onload = function() {
 
 	/* Check box toggle forms functions */
 	function toggleReviewForm() {
-		$('#form-container').toggle()
+		document.getElementById('form-container').style.display = 'block';
 		$('#add-review-form-container').toggle();
 	}
 
 	function toggleImageForm() {
-		$('#form-container').toggle()
+		document.getElementById('form-container').style.display = 'block';
 		$('#add-image-form-container').toggle();
 	}
+
+	function toggleEditForm() {
+		document.getElementById('form-container').style.display = 'block';
+		$('#suggest-edit-form-container').toggle();
+	}
+
+	function toggleFlagForm() {
+		document.getElementById('form-container').style.display = 'block';
+		$('#flag-business-form-container').toggle();
+	}
+
+	function toggleNewBusinessForm() {
+		document.getElementById('form-container').style.display = 'block';
+		$('#add-business-form-container').toggle();
+	}
+
+	function hideFormContainer() {
+		/* hide all forms if no checked boxes are selected */
+	}
+
 
 	let nameRadioSelect = document.getElementById('byName');
 	nameRadioSelect.addEventListener("click", toggleNameMenu);
@@ -38,4 +58,14 @@ window.onload = function() {
 
 	let imageCheckBox = document.getElementById('add-image-checkbox');
 	imageCheckBox.addEventListener("change", toggleImageForm);
+
+	let flagCheckBox = document.getElementById('flag-business-checkbox');
+	flagCheckBox.addEventListener("change", toggleFlagForm);
+
+	let editCheckBox = document.getElementById('edit-business-checkbox');
+	editCheckBox.addEventListener("change", toggleEditForm);
+
+	let newBusinessButton = document.getElementById('add-button');
+	newBusinessButton.addEventListener("click", toggleNewBusinessForm);
+
 }
