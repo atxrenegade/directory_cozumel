40e55ea1bcb39cc992b75fa8790ca381d4f12f38
atxrenegade <harleighabel@gmail.com>
2019-11-20 23:28:48 -0500

    add toggle functionality to reviews form

--- frontend/index.js
+++ frontend/index.js
@@ -16,9 +16,18 @@ window.onload = function() {
 		searchByCategory.style.display = 'none';
 	}
 
+	/* Check box toggle forms functions */
+	function toggleReviewForm() {
+		$('#form-container').toggle()
+		$('#add-review-form-container').toggle();
+	}
+
 	let nameRadioSelect = document.getElementById('byName');
 	nameRadioSelect.addEventListener("click", toggleNameMenu);
 
 	let categoryRadioSelect = document.getElementById('byCategory');
 	categoryRadioSelect.addEventListener("click", toggleCategoryMenu);
+
+	let reviewCheckBox = document.getElementById('add-review-checkbox');
+	reviewCheckBox.addEventListener("change", toggleReviewForm);
 }
