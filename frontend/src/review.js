ae21a6880d170432733172b7de13abd7d9903cd9
atxrenegade <harleighabel@gmail.com>
2019-12-07 16:16:54 -0600

    add constructor method to review class

--- frontend/src/review.js
+++ frontend/src/review.js
@@ -1,3 +1,11 @@
 class Review {
-
+	constructor(content, contributor, contributor_email, date, description, email, id, url){
+		this.content = content;
+		this.contributor = contributor;
+		this.contributor_email = contributor_email;
+		this.description = description;
+		this.id = id;
+		this.rating = rating;
+		this.date = new Date();
+	}
 }
