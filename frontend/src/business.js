e234a830c35cc02662fbca4fe8b98a21a92f340e
atxrenegade <harleighabel@gmail.com>
2019-12-07 15:58:27 -0600

    create constructor, categories and business listing methods for js Business class

--- frontend/src/business.js
+++ frontend/src/business.js
@@ -1,3 +1,16 @@
 class Business {
-	
+	constructor(name, address, overall_rating, phone_number, website) {
+		this.name = name;
+		this.address = address;
+		this.phone_number = phone_number;
+		this.website = website;
+	}
+
+	get categories {
+
+	}
+
+	get businessListing {
+
+	}
 }
