e9a2bce9597acf93c0203e607abe7cead67bdefd
atxrenegade <harleighabel@gmail.com>
2019-12-07 16:04:38 -0600

    add constructor method to image class

--- frontend/src/image.js
+++ frontend/src/image.js
@@ -1,3 +1,11 @@
 class Image {
-
+	constructor(contributor, contributor_email, date, description, email, id, url){
+		this.contributor = contributor;
+		this.contributor_email = contributor_email;
+		this.date = date;
+		this.description = description;
+		this.email = email;
+		this.id = id;
+		this.url = url;
+	}
 }
