7cc3d81ccc6cb7fe4d4fd4ad2eb774d0bea25c00
atxrenegade <harleighabel@gmail.com>
2019-12-16 18:13:18 -0600

    change datatype for Map model lng and lat columns

--- directory-cozumel-backend-api/db/migrate/20191114061810_create_maps.rb
+++ directory-cozumel-backend-api/db/migrate/20191114061810_create_maps.rb
@@ -1,10 +1,10 @@
 class CreateMaps < ActiveRecord::Migration[5.2]
   def change
     create_table :maps do |t|
-      t.integer :lat
-			t.integer :lng
+      t.float :lat, precision: 13, scale: 9
+			t.float :lng, precision: 13, scale: 9
 			t.references :business
-			
+
       t.timestamps
     end
   end
