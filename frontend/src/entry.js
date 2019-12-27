5accfe5fa64b4254ea576f0edebf44e07e233b8c
atxrenegade <harleighabel@gmail.com>
2019-12-26 19:14:17 -0500

    create new js entry class and constructor function

---
+++ frontend/src/entry.js
@@ -0,0 +1,15 @@
+class Entry {
+	constructor(id, dateCreated, busId, entryType, contributor, contributorEmail, dataArray, adminId, resolvedDate, status, notes) {
+		this.id = id;
+		this.dateCreated = dateCreated;
+		this.busId = busId;
+		this.entryType = entryType;
+		this.contributor = contributor;
+		this.contributorEmail = contributorEmail;
+		this.dataArray = dataArray;
+		this.adminId = adminId;
+		this.resolvedDate = resolvedDate;
+		this.status = status;
+		this.notes = notes;
+	}
+}
