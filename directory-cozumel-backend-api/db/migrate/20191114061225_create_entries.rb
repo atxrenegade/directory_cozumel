459b8b6920f4c2ec1b25e072c970c2a907339266
atxrenegade <harleighabel@gmail.com>
2020-01-12 13:54:02 -0500

    change all references to bus_id to business_id in models, migrations and controllers

--- directory-cozumel-backend-api/db/migrate/20191114061225_create_entries.rb
+++ directory-cozumel-backend-api/db/migrate/20191114061225_create_entries.rb
@@ -2,7 +2,7 @@ class CreateEntries < ActiveRecord::Migration[5.2]
   def change
     create_table :entries do |t|
       t.string :entry_type
-			t.integer :bus_id
+			t.integer :business_id
 			t.string :bus_name
       t.string :date
       t.string :contributor
