93e3d869f1a85585cd3528f8717affe713109eea
atxrenegade <harleighabel@gmail.com>
2020-01-19 15:16:39 -0500

    add routes to retrieve model attributes array

--- directory-cozumel-backend-api/config/routes.rb
+++ directory-cozumel-backend-api/config/routes.rb
@@ -3,18 +3,31 @@ Rails.application.routes.draw do
 	post '/index_by_name', to: 'businesses#index_by_name'
 	post '/business', to: 'businesses#show'
 
-	get  '/entries/attributes', to: 'entries#attributes' 
+	get  '/entries/attributes', to: 'entries#attributes'
 	post '/entries/new_object', to: 'entries#new_object'
 	post '/entries/new'
 	post '/entries/update'
 	post '/entries/search', to: 'entries#index_search'
 	post '/entries', to: 'entries#index'
 
+	get '/businesses/attributes', to: 'businesses#attributes'
 	resources :businesses, only: [:create, :update, :delete]
+
+	get '/listings/attributes', to: 'listings#attributes'
 	resources :listings, only: [:create, :update, :delete]
+
+	get '/maps/attributes', to: 'maps#attributes'
 	resources :maps, only: [:create, :update, :delete]
+
+	get '/reviews/attributes', to: 'reviews#attributes'
 	resources :reviews, only: [:create, :update, :delete]
+
+	get '/images/attributes', to: 'images#attributes'
 	resources :images, only: [:create, :update, :delete]
+
+	get '/categories/attributes', to: 'categories#attributes'
 	resources :categories, only: [:index, :create, :update, :delete]
+
+	get '/admins/attributes', to: 'admins#attributes'
 	resources :admins, only: [:create, :update, :delete]
 end
