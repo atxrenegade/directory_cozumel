03d2c17c81b8f898612796a1d6bac58cf43e958e
atxrenegade <harleighabel@gmail.com>
2020-01-20 13:22:58 -0500

    add action to controller to create new instance wiht admin submitted data

--- directory-cozumel-backend-api/app/controllers/images_controller.rb
+++ directory-cozumel-backend-api/app/controllers/images_controller.rb
@@ -1,6 +1,11 @@
 class ImagesController < ApplicationController
 	def attributes
 		attributes = Image.column_names
-		render json: attributes, except: [:id, :created_at, :updated_at] 
+		render json: attributes, except: [:id, :created_at, :updated_at]
+	end
+
+	def create(instObj)
+		instance = Instance.create!(instObj)
+		render json: instance
 	end
 end
