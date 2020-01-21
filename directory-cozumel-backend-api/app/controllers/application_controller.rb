eb51506d04ee82b558f8c100cc7cd024c236e5d9
atxrenegade <harleighabel@gmail.com>
2020-01-21 11:39:14 -0500

    add current_admin and require_admin helper methods to applicationController

--- directory-cozumel-backend-api/app/controllers/application_controller.rb
+++ directory-cozumel-backend-api/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+	helper_method :current_admin, :require_admin
+
+	def current_admin
+		@current_admin || Admin.find(session[:admin_id]) if session[:admin_id]
+	end
+
+	def require_admin
+		redirect_to '/' unless current_admin
+	end
 end
