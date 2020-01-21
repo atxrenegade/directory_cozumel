e3ff0691ba606c20019c5be0f2cf2f918e690fad
atxrenegade <harleighabel@gmail.com>
2020-01-21 12:01:39 -0500

    add helper methods for super and admin roles to application controller

--- directory-cozumel-backend-api/app/controllers/application_controller.rb
+++ directory-cozumel-backend-api/app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 class ApplicationController < ActionController::API
-	helper_method :current_admin, :require_admin
+	helper_method :current_admin, :require_admin, :admin?, :require_super, :super?
 
 	def current_admin
 		@current_admin || Admin.find(session[:admin_id]) if session[:admin_id]
 	end
 
+	def admin?
+		self.role == 'admin' || self.role == 'super'
+	end
+
+	def super?
+		self.role == 'super'
+	end
+
 	def require_admin
-		redirect_to '/' unless current_admin
+		redirect_to '/' unless current_user.admin?
+	end
+
+	def require_super
+		redirect_to '/' unless current_user.super?
 	end
 end
