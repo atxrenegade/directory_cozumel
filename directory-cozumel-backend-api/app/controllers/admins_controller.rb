4cd7f3dcaa41adf3a6b0e653248057d0b23a9426
atxrenegade <harleighabel@gmail.com>
2020-01-21 00:42:05 -0500

    add create action to admins controller

--- directory-cozumel-backend-api/app/controllers/admins_controller.rb
+++ directory-cozumel-backend-api/app/controllers/admins_controller.rb
@@ -1,6 +1,23 @@
 class AdminsController < ApplicationController
 	def attributes
 		attributes = Admin.column_names
-		render json: attributes, except: [:id, :created_at, :updated_at] 
+		render json: attributes, except: [:id, :created_at, :updated_at]
+	end
+
+	def create
+		@admin = Admin.new(admin_params)
+
+		if @admin.save
+			session[admin_id] = @admin.id
+			redirect_to '/'
+		else
+			msg = 'Authorized Admins Only!'
+		end
+		render json: msg
+	end
+	
+	private
+	def admin_params
+		params.require(:admin).permit(:username, :password, :role)
 	end
 end
