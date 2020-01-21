c31d5889b5a05f48a54c77db73b1550148b837bc
atxrenegade <harleighabel@gmail.com>
2020-01-21 00:59:38 -0500

    add create action to sessions controller

--- directory-cozumel-backend-api/app/controllers/sessions_controller.rb
+++ directory-cozumel-backend-api/app/controllers/sessions_controller.rb
@@ -1,3 +1,14 @@
 class SessionsController < ApplicationController
 
+	def create
+
+		@admin = Admin.find_by_username(params[:session][:password])
+		if @admin && @admin.authenticate(params[:session][:password])
+			session[:admin_id] = @admin.id
+			msg = 'Admin Successfully Created'
+		else
+			msg = 'New Admin Creation Failed'
+		end
+		render json: msg
+	end		
 end
