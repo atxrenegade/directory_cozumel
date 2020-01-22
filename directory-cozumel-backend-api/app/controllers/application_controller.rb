60bfec2ebaffcf4a3db1efce53c6c3a0b6fc7bbd
atxrenegade <harleighabel@gmail.com>
2020-01-21 23:15:23 -0500

    build out Readme delete duplicate file

--- directory-cozumel-backend-api/app/controllers/application_controller.rb
+++ directory-cozumel-backend-api/app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::API
+	# before_action :require_super
+	def attributes
+		attributes = Admin.column_names
+		render json: attributes, except: [:id, :created_at, :updated_at]
+	end
+
+	def create
+		@admin = Admin.new(admin_params)
+
+		if @admin.save
+			session[:admin_id] = @admin.id
+			redirect_to '/'
+			msg = 'New Admin Created Successfully'
+		else
+			msg = 'Authorized Admins Only!'
+		end
+		render json: msg
+	end
+
+	private
+	def admin_params
+		params.require(:admin).permit(:username, :password, :role)
+	end
 end
