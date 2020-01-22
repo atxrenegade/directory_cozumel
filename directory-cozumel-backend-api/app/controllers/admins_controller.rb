03ddef2e8ad44ade9abc4d579d5dfc2b785ba693
atxrenegade <harleighabel@gmail.com>
2020-01-21 23:20:50 -0500

    build out admins_controller with helper methods and include helpers module

--- directory-cozumel-backend-api/app/controllers/admins_controller.rb
+++ directory-cozumel-backend-api/app/controllers/admins_controller.rb
@@ -1,6 +1,29 @@
 class AdminsController < ApplicationController
+	include ActionController::Helpers
+	helper_method :current_admin, :require_admin, :admin?, :require_super, :super?
+
 	def attributes
 		attributes = Admin.column_names
-		render json: attributes, except: [:id, :created_at, :updated_at] 
+		render json: attributes, except: [:id, :created_at, :updated_at]
+	end
+	
+	def current_admin
+		@current_admin || Admin.find(session[:admin_id]) if session[:admin_id]
+	end
+
+	def admin?
+		self.role == 'admin' || self.role == 'super'
+	end
+
+	def super?
+		self.role == 'super'
+	end
+
+	def require_admin
+		redirect_to '/' unless current_user.admin?
+	end
+
+	def require_super
+		redirect_to '/' unless current_user.super?
 	end
 end
