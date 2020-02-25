1af3832cb383a1fbc3da2ae3edbae1250a3021d9
atxrenegade <harleighabel@gmail.com>
2020-02-25 12:28:29 -0600

    refactor for consistency

--- directory-cozumel-backend-api/app/controllers/entries_controller.rb
+++ directory-cozumel-backend-api/app/controllers/entries_controller.rb
@@ -1,7 +1,7 @@
 class EntriesController < ApplicationController
 	# before_action :require_admin, except: [:create]
 	def show
-		entry = Entry.find_by_id(params['id'])
+		entry = Entry.find_by_id(params[:id])
 		render json: entry
 	end
 
@@ -12,7 +12,7 @@ class EntriesController < ApplicationController
 	end
 
 	def build_object_from_entry
-		entry = Entry.find_by_id(params['id'])
+		entry = Entry.find_by_id(params[:id])
 		entry.convert_to_object()
 		if entry.persisted?
 			response = { msg: 'Object Saved'}
@@ -28,28 +28,28 @@ class EntriesController < ApplicationController
 	end
 
 	def index
-		entries = Entry.collect(params['search_type'], params['auth_type'])
+		entries = Entry.collect(params[:search_type], params[:auth_type])
 		render json: entries
 	end
 
 	def search
-		property_param = params['property'][0].gsub('-', '_')
-		entries = Entry.search_entries(property_param, params['search_val'])
+		property_param = params[:property][0].gsub('-', '_')
+		entries = Entry.search_entries(property_param, params[:search_val])
 		render json: entries
 	end
 
 	def update
 		entry = Entry.find(params[:id])
 		update_vals = {}
-		update_vals['status'] = params[:status] if params[:status].present?
+		update_vals[:status] = params[:status] if params[:status].present?
 
-		update_vals['admin_id']  = params[:admin_id] if params[:admin_id].present?
+		update_vals[:admin_id]  = params[:admin_id] if params[:admin_id].present?
 
-		update_vals['data_object'] = params[:data_object] if params[:data_object].present?
+		update_vals[:data_object] = params[:data_object] if params[:data_object].present?
 
-		update_vals['resolved_date'] = params['resolved_date'] if params['resolved_date'].present?
+		update_vals[:resolved_date] = params[:resolved_date] if params[:resolved_date].present?
 
-		update_vals['notes'] = params[:notes] if params[:notes].present?
+		update_vals[:notes] = params[:notes] if params[:notes].present?
 
 		if entry.update!(update_vals)
 			response = { msg: 'Entry Successfully Updated'}
