9396780630891f9f425cc0dafd9e0e042195042c
atxrenegade <harleighabel@gmail.com>
2020-02-26 16:34:33 -0600

    build out helper methods and update actions in admins controller

--- directory-cozumel-backend-api/app/controllers/admins_controller.rb
+++ directory-cozumel-backend-api/app/controllers/admins_controller.rb
@@ -3,15 +3,15 @@ class AdminsController < ApplicationController
 	include ActionController::Helpers
 	helper_method :current_admin, :require_admin, :admin?, :require_super, :super?
 
-	def show
-		admin = Admin.find_by_id(params['id'])
-		render json: admin
-	end
-
 	def attributes
 		columnsToExclude = ['id', 'created_at', 'updated_at']
 		attributes = Admin.attribute_names - columnsToExclude
 		render json: attributes
+	end
+
+	def show
+		admin = Admin.find_by_id(params['id'])
+		render json: admin
 	end
 
 	def create
@@ -19,11 +19,32 @@ class AdminsController < ApplicationController
 		render json: instance
 	end
 
-	def edit
-	  admin = Admin.find(params[:id])
-		render json: admin
+	def update 
+		admin = Admin.find(params[:id])
+		update_vals = {}
+		update_vals[:username] = params[:username] if params[:username].present?
+		update_vals[:password]  = params[:password] if params[:password].present?
+		update_vals[:role] = params[:role] if params[:role].present?
+		update_vals[:status] = params[:status] if params[:status].present?
+
+		admin.update!(update_vals) ? response = { msg: 'Admin Successfully Updated'} : response = { msg: 'Admin Failed to Update'}
+		render json: response
 	end
 
+	def destroy
+		require_super()
+		admin = Admin.find(params[:id])
+		admin.destroy
+		response = {}
+		if admin.destroyed?
+			response['msg'] = 'Admin Deleted!'
+		else
+			response['msg'] = 'Admin Failed to Delete!'
+		end
+		render json: response
+	end
+
+	helpers
 	def current_admin
 		@current_admin || Admin.find(session[:admin_id]) if session[:admin_id]
 	end
@@ -42,19 +63,6 @@ class AdminsController < ApplicationController
 
 	def require_super
 		redirect_to '/' unless current_admin.super?
-	end
-
-	def destroy
-		require_super()
-		admin = Admin.find(params[:id])
-		admin.destroy
-		response = {}
-		if admin.destroyed?
-			response['msg'] = 'Admin Deleted!'
-		else
-			response['msg'] = 'Admin Failed to Delete!'
-		end
-		render json: response
 	end
 
 	private
