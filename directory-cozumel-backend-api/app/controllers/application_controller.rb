5627c7dbe7031a547b2bde73b0d2647b591d6695
atxrenegade <harleighabel@gmail.com>
2020-02-26 18:54:32 -0600

    remove admins actions from application_controller

--- directory-cozumel-backend-api/app/controllers/application_controller.rb
+++ directory-cozumel-backend-api/app/controllers/application_controller.rb
@@ -1,20 +1,2 @@
 class ApplicationController < ActionController::Base
-	# before_action :require_super
-
-	def attributes
-		attributes = Admin.column_names
-		render json: attributes, except: [:id, :created_at, :updated_at]
-	end
-
-	def create
-		@admin = Admin.new(admin_params)
-		if @admin.save
-			session[:admin_id] = @admin.id
-			redirect_to '/'
-			msg = 'New Admin Created Successfully'
-		else
-			msg = 'Authorized Admins Only!'
-		end
-		render json: msg
-	end
 end
