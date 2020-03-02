2b412736fa26b16a28dae496b856ef4ccac78719
atxrenegade <harleighabel@gmail.com>
2020-03-01 21:17:41 -0600

    add storageModule to replace global variables

---
+++ frontend/src/sessionStorage/appStorage.js
@@ -0,0 +1,60 @@
+function appStorage(){
+
+	this.setCats = function(data) {
+		sessionStorage.setItem('cats', data);
+	}
+
+	this.getCats = function(data) {
+		return sessionStorage['cats'];
+	}
+
+	this.setEntries = function(data) {
+		sessionStorage.setItem('entries', data);
+	}
+
+	this.getEntries = function(data) {
+		return sessionStorage['entries'];
+	}
+
+	this.setAttributes = function(data) {
+		sessionStorage.setItem('attributes', data);
+	}
+
+	this.getAttributes = function(data) {
+		return sessionStorage['attributes'];
+	}
+
+	this.setAllBusinesses = function(data) {
+		sessionStorage.setItem('AllBusinesses', data);
+	}
+
+	this.getAllBusinesses = function(data) {
+		return sessionStorage['AllBusinesses'];
+	}
+
+	this.setResponse = function(data) {
+		sessionStorage.setItem('response', data);
+	}
+
+	this.getResponse = function(data) {
+		return sessionStorage['response'];
+	}
+
+	this.setResult = function(data) {
+		sessionStorage.setItem('result', data);
+	}
+
+	this.getResult = function(data) {
+		return sessionStorage['result'];
+	}
+
+	function setAdmin(data)
+		sessionStorage.setItem('admin', data);
+	end
+
+	function getAdmin
+		return sessionStorage['admin']
+	end
+}
+
+module.exports = appStorage;
