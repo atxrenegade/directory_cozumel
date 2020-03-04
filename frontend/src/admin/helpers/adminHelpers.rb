a548e2be01690ee294bd7b7e15ebd262da557925
atxrenegade <harleighabel@gmail.com>
2020-03-04 14:16:25 -0600

    move helpers to admin/helpers/ folder

---
+++ frontend/src/admin/helpers/adminHelpers.rb
@@ -0,0 +1,21 @@
+module adminHelpers
+	def current_admin
+		@current_admin ||= Admin.find(session[:id]) if session[:id]
+	end
+
+	def admin?
+		self.role == 'admin' && self.status == 'active'|| self.role == 'super' && self.status == 'active'
+	end
+
+	def super?
+		self.role == 'super' && self.status == 'active'
+	end
+
+	def require_admin
+		redirect_to '/' unless @current_admin.admin?
+	end
+
+	def require_super
+		redirect_to '/' unless @current_admin.super?
+	end
+end
