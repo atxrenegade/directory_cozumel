eaade01fd1f5dab531872afb0579a2f1ac9a4f90
atxrenegade <harleighabel@gmail.com>
2020-06-16 19:52:22 -0500

    debug userVariables module export

--- frontend/src/elementVariables/userVar.js
+++ frontend/src/elementVariables/userVar.js
@@ -1,37 +1,37 @@
 	// User Variables
-	function userVariables(){
+	function userVar(){
 		return {
 			/* searchbar elements */
-			searchByName: document.getElementById('js-search-by-name');
-			searchByCategory: document.getElementById('js-search-by-category');
-			nameRadioSelect: document.getElementById('js-radio-by-name');
-			categoryRadioSelect: document.getElementById('js-radio-by-category');
-			searchCategoryMenu: document.getElementById('js-search-category-menu');
-			searchNameField: document.getElementById('js-search-name-text-field');
+			searchByName: document.getElementById('js-search-by-name'),
+			searchByCategory: document.getElementById('js-search-by-category'),
+			nameRadioSelect: document.getElementById('js-radio-by-name'),
+			categoryRadioSelect: document.getElementById('js-radio-by-category'),
+			searchCategoryMenu: document.getElementById('js-search-category-menu'),
+			searchNameField: document.getElementById('js-search-name-text-field'),
 
 			/* checkbox elements */
-			reviewCheckBox: document.getElementById('js-add-review-checkbox');
-			imageCheckBox: document.getElementById('js-add-image-checkbox');
-			flagCheckBox: document.getElementById('js-flag-business-checkbox');
-			editCheckBox: document.getElementById('js-edit-business-checkbox');
+			reviewCheckBox: document.getElementById('js-add-review-checkbox'),
+			imageCheckBox: document.getElementById('js-add-image-checkbox'),
+			flagCheckBox: document.getElementById('js-flag-business-checkbox'),
+			editCheckBox: document.getElementById('js-edit-business-checkbox'),
 
 			/* business listing elements */
-			businessListings: document.getElementById('js-listing-show');
-			listingMenu: document.getElementById('js-listing-menu');
+			businessListings: document.getElementById('js-listing-show'),
+			listingMenu: document.getElementById('js-listing-menu'),
 
 			/* add business form elements */
-			newBusinessButton: document.getElementById('js-add-business-button');
-			newBusinessForm: document.getElementById('js-new-business-form');
+			newBusinessButton: document.getElementById('js-add-business-button'),
+			newBusinessForm: document.getElementById('js-new-business-form'),
 
 			/* admin hidden button element */
-			hiddenAdminButton: document.getElementById('js-admin-hidden-button');
+			hiddenAdminButton: document.getElementById('js-admin-hidden-button'),
 
 			/* admin login panel elements */
-			adminPanelLogin: document.getElementById('js-admin-login-button');
+			adminPanelLogin: document.getElementById('js-admin-login-button'),
 
 			/* container elements */
 			listingsContainer: document.getElementById('listings-container')
 		}
 	}
 
-	export { userVariables };
+	export { userVar };
