03b093bdd9c3329ef8d7481d25b19cd8f5d5785f
atxrenegade <harleighabel@gmail.com>
2020-06-16 19:52:03 -0500

    debug adminVariables module export

--- frontend/src/elementVariables/adminVar.js
+++ frontend/src/elementVariables/adminVar.js
@@ -1,35 +1,39 @@
 	// Admin Variables
-	const loginContainer = document.getElementById('js-admin-login-container')
-	const tableContainer = document.getElementById('js-admin-panel-container')
+	function adminVar() {
+		return {
+			loginContainer: document.getElementById('js-admin-login-container'),
+			tableContainer: document.getElementById('js-admin-panel-container'),
 
-	/* admin details table elements */
-	const smResolvedIndexButton = document.getElementById('admin-resolved-index-button')
-	const smPendingIndexButton = document.getElementById('admin-pending-index-button')
-	const rejectButton = document.getElementById('admin-reject-button')
-	const approveButton = document.getElementById('admin-approve-button')
-	const addNotesButton = document.getElementById('admin-notes-button')
-	const newNotesSubmitButton = document.getElementById('admin-submit-notes-field')
-	const superAdminMenuButton = document.getElementById('js-admin-show-super-admin')
-	/* super admin auth panel elements */
-	const createAdminButton = document.getElementById('js-admin-create-admin')
-	const editAdminButton = document.getElementById('js-admin-edit-admin')
-	const deleteAdminButton = document.getElementById('js-admin-delete-admin')
-	/* admin entry details panel elements */
-	const bigPendingIndexButton = document.getElementById('admin-show-pending-button')
-	const bigResolvedIndexButton = document.getElementById('admin-show-resolved-button')
-	const searchButton = document.getElementById('js-admin-search-entries')
+			/* admin details table elements */
+			 smResolvedIndexButton: document.getElementById('admin-resolved-index-button'),
+			 smPendingIndexButton: document.getElementById('admin-pending-index-button'),
+			 rejectButton: document.getElementById('admin-reject-button'),
+			 approveButton: document.getElementById('admin-approve-button'),
+			 addNotesButton: document.getElementById('admin-notes-button'),
+			 newNotesSubmitButton: document.getElementById('admin-submit-notes-field'),
+			 superAdminMenuButton: document.getElementById('js-admin-show-super-admin'),
+			/* super admin auth panel elements */
+			 createAdminButton: document.getElementById('js-admin-create-admin'),
+			 editAdminButton: document.getElementById('js-admin-edit-admin'),
+			 deleteAdminButton: document.getElementById('js-admin-delete-admin'),
+			/* admin entry details panel elements */
+			 bigPendingIndexButton: document.getElementById('admin-show-pending-button'),
+			 bigResolvedIndexButton: document.getElementById('admin-show-resolved-button'),
+			 searchButton: document.getElementById('js-admin-search-entries'),
 
-	/* super admin CRUD panel elements */
-	const superAdminCRUDMenu = document.getElementById('js-super-admin-modify-menu')
+			/* super admin CRUD panel elements */
+			 superAdminCRUDMenu: document.getElementById('js-super-admin-modify-menu'),
 
-	/* admin login/logout elements */
-	const logoutButton = document.getElementById('js-admin-logout-button')
+			/* admin login/logout elements */
+			 logoutButton: document.getElementById('js-admin-logout-button'),
 
-	/* elements to reset on admin logout */
-	const superAdminPanel = document.getElementById('js-admin-super-admin-open')
-	const indexTable = document.getElementById('admin-entry-table');
-	const detailsTable = document.getElementById('entry-details-tables');
-	const notesForm = document.getElementById('admin-notes-form')
-	const entryNotes = document.getElementById('js-entry-notes')
+			/* elements to reset on admin logout */
+			 superAdminPanel: document.getElementById('js-admin-super-admin-open'),
+			 indexTable: document.getElementById('admin-entry-table'),
+			 detailsTable: document.getElementById('entry-details-tables'),
+			 notesForm: document.getElementById('admin-notes-form'),
+			 entryNotes: document.getElementById('js-entry-notes')
+			}
+		}
 
-	export { loginContainer, tableContainer, smResolvedIndexButton, smPendingIndexButton, rejectButton, approveButton, addNotesButton, newNotesSubmitButton, superAdminMenuButton, createAdminButton,  editAdminButton, deleteAdminButton, bigPendingIndexButton, bigResolvedIndexButton, searchButton, superAdminCRUDMenu, logoutButton, superAdminPanel, indexTable, detailsTable, notesForm, entryNotes }
+		export { adminVar };
