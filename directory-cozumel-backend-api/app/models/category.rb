49e635c9025f0612c4ccc555c77aa3457cfba480
atxrenegade <harleighabel@gmail.com>
2020-06-25 21:35:20 -0500

    create Category.find_by_name_or_by_nombre

--- directory-cozumel-backend-api/app/models/category.rb
+++ directory-cozumel-backend-api/app/models/category.rb
@@ -4,5 +4,14 @@ class Category < ApplicationRecord
 
 	def business_names
 		self.businesses.pluck(:name)
-	end	
+	end
+
+	def find_by_name_or_nombre(cat_name)
+		if Category.find_by(name: cat_name).exists?
+			cat = Category.find_by(name: cat_name)
+		else
+			cat = Category.find_by(nombre: cat_name)
+		end
+		return cat	
+	end
 end
