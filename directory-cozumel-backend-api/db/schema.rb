d9a23643fe20feb77554771885d4ef9708cb8841
atxrenegade <harleighabel@gmail.com>
2020-07-20 21:02:20 -0500

    add query string to maps model in schema

--- directory-cozumel-backend-api/db/schema.rb
+++ directory-cozumel-backend-api/db/schema.rb
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2020_06_25_190111) do
   create_table "maps", force: :cascade do |t|
     t.float "lat"
     t.float "lng"
+    t.string "query_string"
     t.integer "business_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
