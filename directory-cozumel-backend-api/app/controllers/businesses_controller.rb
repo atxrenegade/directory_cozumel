cb1870295a1dba41a64b9f03a330463c4b8fc8ef
atxrenegade <harleighabel@gmail.com>
2020-07-25 17:23:43 -0500

    return updated_at in listing json

--- directory-cozumel-backend-api/app/controllers/businesses_controller.rb
+++ directory-cozumel-backend-api/app/controllers/businesses_controller.rb
@@ -16,7 +16,7 @@ class BusinessesController < ApplicationController
 		business = Business.find_by_name(params[:name])
 		if business.present?
 			render json: business.to_json(:include => {
-				:listing => {:except => [:id, :created_at, :updated_at, :business_id]},
+				:listing => {:except => [:id, :created_at, :business_id]},
 				:categories => {:only => [:name, :nombre]},
 				:map => {:only => [:lat, :lng]},
 				:images => {:except => [:id, :contributor_email, :business_id, :created_at, :updated_at]},
@@ -29,7 +29,7 @@ class BusinessesController < ApplicationController
 	end
 
 	def index_by_category
-		if params[:category_name] == 'Sustainable Businesses' ||  params[:category_name] == 'Empresas Sostenibles'
+		if params[:category_name] == 'Sustainable Businesses' ||  params[:category_name] == 'Negocio Sustentable'
 			businesses = Listing.filter_by_sustainable
 		else 
 			businesses = BusinessCategory.filter_by_category(params[:lang], params[:category_name])	
@@ -38,7 +38,7 @@ class BusinessesController < ApplicationController
 			businesses_array = []
 			businesses.each do |el|
 				listing_element = el.to_json(:include => {
-					:listing => {:except => [:id, :created_at, :updated_at, :business_id]},
+					:listing => {:except => [:id, :created_at, :business_id]},
 					:categories => {:only => [:name, :nombre]}
 				})
 				businesses_array.push(listing_element)
@@ -55,7 +55,7 @@ class BusinessesController < ApplicationController
 			businesses_array = []
 			businesses.each do |el|
 				listing_element = el.to_json(:include => {
-					:listing => {:except => [:id, :created_at, :updated_at, :business_id]},
+					:listing => {:except => [:id, :created_at, :business_id]},
 					:categories => {:only => [:name, :nombre]}
 				})
 				businesses_array.push(listing_element)
