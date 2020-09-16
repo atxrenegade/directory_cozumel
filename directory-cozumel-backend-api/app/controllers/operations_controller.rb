47bd76e9c07b04b6b34167cd1031ca6d3a3e3319
atxrenegade <harleighabel@gmail.com>
2020-09-16 14:18:52 -0500

    add operations show, edit and delete actions

--- directory-cozumel-backend-api/app/controllers/operations_controller.rb
+++ directory-cozumel-backend-api/app/controllers/operations_controller.rb
@@ -2,13 +2,15 @@ class OperationsController < ApplicationController
   before_action :set_operation, only: [:show, :edit, :update, :destroy]
 
 
-  # GET /operations/1
   def show
+    operation = Operation.find_by_id(params['id'])
+    render json: operation
   end
-
 
   # GET /operations/1/edit
   def edit
+    operation = Operation.find(params['id'])
+    render json: operation
   end
 
   # POST /operations
@@ -31,11 +33,27 @@ class OperationsController < ApplicationController
     end
   end
 
-  # DELETE /operations/1
   def destroy
     @operation.destroy
-    redirect_to operations_url, notice: 'Operation was successfully destroyed.'
+    response = {} 
+    if operation.destroyed?
+			response['msg'] = 'Operation Deleted!'
+		else
+			response['msg'] = 'Operation Failed to Delete!'
+		end
+		render json: response
   end
+
+  def index_associated
+		operations = Operation.where(business_id: params['business_id']).all
+		render json: operations
+  end
+  
+   def attributes
+		columnsToExclude = ['id', 'created_at', 'updated_at']
+		attributes = Operation.attribute_names - columnsToExclude
+		render json: attributes
+	end
 
   private
     # Use callbacks to share common setup or constraints between actions.
